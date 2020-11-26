964e354773301de9e08d7631859cb9ce2ab31751
Jury Andrea Donofrio <j.donofrio@campus.unimib.it>
2020-11-26 17:54:47 +0100

    modifiche

--- src/main/java/ilaria/travistest/App.java
+++ src/main/java/ilaria/travistest/App.java
@@ -13,8 +13,6 @@ public class App
     	} else {
     		System.out.println( "What's your name????");
     		System.out.println( "I would love to see travis working...");
-    		System.out.println( "Hello World");
-		System.out.println( "Hello Jury");
     	}
     }
 }
